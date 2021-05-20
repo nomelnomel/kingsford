7ef9fcb9753e9552ce8165354acf16bd6ee489df
Nomel <nomelnomel@gmail.com>
2021-05-20 20:11:32 +0300

    first commit

---
+++ components/MainPage/MainSlide.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MainSlide"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/locations.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  страница адресов
+</div>
+</template>
+
+<script>
+export default {
+  name: 'location'
+}
+</script>
+
+<style scoped>
+
+</style>

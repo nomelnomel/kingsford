1e7858f17987bcd54cc9bd76065b62e83022a146
Nomel <nomelnomel@gmail.com>
2021-05-20 20:13:29 +0300

    upload 2

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="main">
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.main{
+  margin: 0 100px;
+  position: relative;
+  overflow-x: hidden;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+img{
+  width: 100%;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <MainSlide />
+    <About />
+    <SignUp/>
+  </div>
+</template>
+
+<script>
+import MainSlide from '../components/MainPage/MainSlide'
+import About from '../components/MainPage/About'
+import SignUp from '../components/MainPage/SignUp'
+export default {
+  components: {
+    MainSlide,
+    About,
+    SignUp
+  }
+}
+</script>
+
+<style>
+
+.container {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+
+</style>

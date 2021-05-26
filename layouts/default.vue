fe4b032ed58e4e486c53a80424395a2e7c4f0e09
Nomel <nomelnomel@gmail.com>
2021-05-26 20:06:25 +0300

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,18 +17,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 @import url("https://use.typekit.net/pwo8bya.css");
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
